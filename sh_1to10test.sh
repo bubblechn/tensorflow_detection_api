c0f5b97a46454aeb2abf51b0b99e3da120db49c5
bubbleit <bubbleit@163.com>
2018-10-20 22:36:39 +0800

    test pretrained models' time of 1 to 10 objects

---
+++ sh_1to10test.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+for i in $(seq 1 10)
+do 
+	python 1to10test_new.py --image_path /home/yanjieliu/dataset/img_test/p1.jpg --model_name ssd_resnet50_v1_fpn_shared_box_predictor_640x640_coco14_sync_2018_07_03
+done
+
+for i in $(seq 1 10)
+do 
+	python 1to10test_new.py --image_path /home/yanjieliu/dataset/img_test/p2.jpg --model_name ssd_resnet50_v1_fpn_shared_box_predictor_640x640_coco14_sync_2018_07_03
+done
+
+for i in $(seq 1 10)
+do 
+	python 1to10test_new.py --image_path /home/yanjieliu/dataset/img_test/p3.jpg --model_name ssd_resnet50_v1_fpn_shared_box_predictor_640x640_coco14_sync_2018_07_03
+done
+
+for i in $(seq 1 10)
+do 
+	python 1to10test_new.py --image_path /home/yanjieliu/dataset/img_test/p4.jpg --model_name ssd_resnet50_v1_fpn_shared_box_predictor_640x640_coco14_sync_2018_07_03
+done
+
+for i in $(seq 1 10)
+do 
+	python 1to10test_new.py --image_path /home/yanjieliu/dataset/img_test/p5.jpg --model_name ssd_resnet50_v1_fpn_shared_box_predictor_640x640_coco14_sync_2018_07_03
+done
+
+for i in $(seq 1 10)
+do 
+	python 1to10test_new.py --image_path /home/yanjieliu/dataset/img_test/p6.jpg --model_name ssd_resnet50_v1_fpn_shared_box_predictor_640x640_coco14_sync_2018_07_03
+done
+
+for i in $(seq 1 10)
+do 
+	python 1to10test_new.py --image_path /home/yanjieliu/dataset/img_test/p7.jpg --model_name ssd_resnet50_v1_fpn_shared_box_predictor_640x640_coco14_sync_2018_07_03
+done
+
+for i in $(seq 1 10)
+do 
+	python 1to10test_new.py --image_path /home/yanjieliu/dataset/img_test/p8.jpg --model_name ssd_resnet50_v1_fpn_shared_box_predictor_640x640_coco14_sync_2018_07_03
+done
+
+for i in $(seq 1 10)
+do 
+	python 1to10test_new.py --image_path /home/yanjieliu/dataset/img_test/p9.jpg --model_name ssd_resnet50_v1_fpn_shared_box_predictor_640x640_coco14_sync_2018_07_03
+done
+
+for i in $(seq 1 10)
+do 
+	python 1to10test_new.py --image_path /home/yanjieliu/dataset/img_test/p10.jpg --model_name ssd_resnet50_v1_fpn_shared_box_predictor_640x640_coco14_sync_2018_07_03
+done
+
